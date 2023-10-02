83fd82def745a230542c72a877e7602f67fa7b2e
Patrick-Tomic <pat12tomic@gmail.com>
2023-10-02 15:44:51 -0400

    functionality of status change once clicked, partially need to not hit logo but around logo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- function book(name, author, pageCount, read){
+function book(name, author, pageCount, read){
     this.name = name
     this.author= author
     this.pageNum = pageCount
@@ -7,11 +7,10 @@
 book.prototype.info = function(){
     console.log(`${this.name} by ${this.author} with ${this.pageNum} pages. `) 
 }
-
 const lotr = new book("lotr","J.r.r Tolkien",10000,'True');
 lotr.info();   
     let bookArray = [];
-   
+
     const bookForm = document.getElementById("bookForm");
     bookForm.addEventListener('submit',(e)=>{
          e.preventDefault();  
@@ -20,24 +19,29 @@ lotr.info();
         const pageCount = document.getElementById('pageCount');
         const isRead = document.getElementById('isRead');
        let bool =isRead.checked;
-      
-   
-        
+
+
+
           const obj = new book(title.value,author.value,pageCount.value,bool);
           bookArray.push(obj);
         addNewBook(obj);
        title.value ="";
        author.value = "";
         pageCount.value = "";
-       console.log(bookArray);
+
+      
     });
-    
+    const button = document.createElement('button');
+    button.innerHTML = "delete";
+    button.setAttribute('style','width:8vw;height:5vh;color:white;background-color:#007FFF;padding:8px;');
+
 function deleteRow(el){
     var tbl = el.parentNode.parentNode.parentNode;
     var row = el.parentNode.parentNode.rowIndex;
 
     tbl.deleteRow(row);
 }
+
 
 function addNewBook(book){
     const gridWrap = document.getElementById("gridWrapper");
@@ -46,9 +50,9 @@ function addNewBook(book){
     const tr = table.insertRow(-1);
 
     let objVal = Object.values(book);
-   
+    
+
     for(let i=0;i<=objVal.length;i++){
-       
         const button = document.createElement('button');
         button.innerHTML = "delete";
         button.setAttribute('style','width:8vw;height:5vh;color:white;background-color:#007FFF;padding:8px;');
@@ -64,35 +68,38 @@ function addNewBook(book){
            }
         const td = tr.insertCell(i);
         td.setAttribute('style','width:8vw;padding: 15px;border-bottom:dashed black; text-align:center;');
+        td.innerHTML = objVal[i];
         if(i === 3){
-          
-         
+
+            
+
 
             if(objVal[i]==true){
-                objVal[i] = '<svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21.1107" height="5.00231" transform="matrix(0.762286 0.64724 -0.449986 0.893036 3.5004 12.4375)" fill="#02FF2B"/> <rect width="28.3421" height="4.47" transform="matrix(0.475419 -0.879759 0.738497 0.674256 15.0821 25.7802)" fill="#02FF2B"/> </svg>';
-              
+                td.classList.add('check');
             }else{
-              objVal[i] ='<svg width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="31.898" height="5.70542" transform="matrix(0.722122 -0.691766 0.857633 0.514262 1.16681 22.0659)" fill="#FF0404"/> <rect width="32.6729" height="5.58018" transform="matrix(0.779204 0.62677 -0.813795 0.581152 4.54114 0.677856)" fill="#FF0404"/> </svg>';
-             
+                td.classList.add('cross');
              }
-           
+            document.addEventListener('click',(event)=>{
+                var {target} = event;
+                if(target.classList.contains('check')){
+                    target.classList.remove('check');
+                    target.classList.add('cross');
+                     td.innerHTML = '<svg class="cross" width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="31.898" height="5.70542" transform="matrix(0.722122 -0.691766 0.857633 0.514262 1.16681 22.0659)" fill="#FF0404"/> <rect width="32.6729" height="5.58018" transform="matrix(0.779204 0.62677 -0.813795 0.581152 4.54114 0.677856)" fill="#FF0404"/> </svg>'; 
+                }else if(target.classList.contains("cross")){
+                    target.classList.remove('cross');
+                    target.classList.add('check');
+                   td.innerHTML = '<svg class="check" width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21.1107" height="5.00231" transform="matrix(0.762286 0.64724 -0.449986 0.893036 3.5004 12.4375)" fill="#02FF2B"/> <rect width="28.3421" height="4.47" transform="matrix(0.475419 -0.879759 0.738497 0.674256 15.0821 25.7802)" fill="#02FF2B"/> </svg>'; 
+                }
+            });
         }
-        
-        td.innerHTML = objVal[i];
+        if(td.classList.contains('check')){
+            td.innerHTML = '<svg class="check" width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21.1107" height="5.00231" transform="matrix(0.762286 0.64724 -0.449986 0.893036 3.5004 12.4375)" fill="#02FF2B"/> <rect width="28.3421" height="4.47" transform="matrix(0.475419 -0.879759 0.738497 0.674256 15.0821 25.7802)" fill="#02FF2B"/> </svg>';
+        }else if(td.classList.contains('cross')){
+            td.innerHTML = '<svg class="cross" width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="31.898" height="5.70542" transform="matrix(0.722122 -0.691766 0.857633 0.514262 1.16681 22.0659)" fill="#FF0404"/> <rect width="32.6729" height="5.58018" transform="matrix(0.779204 0.62677 -0.813795 0.581152 4.54114 0.677856)" fill="#FF0404"/> </svg>';
+        }
     }
+ 
    
     tableDiv.appendChild(table);
     gridWrap.append(tableDiv);
-}
-const tds = document.querySelectorAll("tr[3]");
-tds.forEach(td,toggleRead(td));
-function toggleRead(td){
-td.addEventListener('onclick',()=>{
-    if(td.innerHTML =='<svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21.1107" height="5.00231" transform="matrix(0.762286 0.64724 -0.449986 0.893036 3.5004 12.4375)" fill="#02FF2B"/> <rect width="28.3421" height="4.47" transform="matrix(0.475419 -0.879759 0.738497 0.674256 15.0821 25.7802)" fill="#02FF2B"/> </svg>'){
-    td.innerHTML='<svg width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="31.898" height="5.70542" transform="matrix(0.722122 -0.691766 0.857633 0.514262 1.16681 22.0659)" fill="#FF0404"/> <rect width="32.6729" height="5.58018" transform="matrix(0.779204 0.62677 -0.813795 0.581152 4.54114 0.677856)" fill="#FF0404"/> </svg>';
-  }else{
-    console.log('bam');
-    td.innerHTML ='<svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21.1107" height="5.00231" transform="matrix(0.762286 0.64724 -0.449986 0.893036 3.5004 12.4375)" fill="#02FF2B"/> <rect width="28.3421" height="4.47" transform="matrix(0.475419 -0.879759 0.738497 0.674256 15.0821 25.7802)" fill="#02FF2B"/> </svg>';
-  }
-});
 }
